9e2431cdff09d34364e7042392fa38330a3d01cb
Alessandro Cardarelli <kardarelli.ale222@gmail.com>
2023-05-12 15:59:05 +0200

    modficia 1

--- prova2.js
+++ prova2.js
@@ -1,3 +1,41 @@
-let a=10
-let b=20
-console.log("somma", a+b)
+const a=10;
+const b="ciao";
+const c= false;
+const d = [];
+const e = {};
+const f = new Date;
+function g(){};
+const h = undefined;
+console.log(typeof a,
+    typeof b,
+    typeof c,
+    typeof d,
+    typeof e,
+    typeof f,
+    typeof g,
+    typeof h
+    )
+
+    const carrello = {
+        prodotti : [
+            {
+                descrizione: "carne",
+                prezzo: 500,
+                qty: 10
+            },
+            {
+                descrizione: "latte",
+                prezzo: 1700,
+                qty: 1
+            }
+    
+        ],
+        getTotal(){
+            for(let i=0;1<prodotti.length;i++){
+                const prodotto = this.prodotti[i];
+                totale += prodotto.prezzo * prodotto.qty;
+            }
+            return totale
+        }
+    }
+    console.log("stampatot", carrello.getTotal)
